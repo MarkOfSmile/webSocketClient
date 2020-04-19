fb704993158379e4bec463fad2eab7be054ef54c
yunkang <yunkang@yunkang>
2020-04-19 14:05:26 +0800

    commit by markOfSmile

--- src/com/jack/test/ken.java
+++ src/com/jack/test/ken.java
@@ -3,9 +3,9 @@ package com.jack.test;
 public class ken {
 
 	public static void main(String[] args) {
-		//modify in eclipse1
+		//modify in eclipse111
 		System.out.println("在本地修改和云端会出现冲突");
-		//modify in eclipse1
+		//modify in eclipse111
 		System.out.println("在云端修改和本地会出现冲突");
 	}
 
