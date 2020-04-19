a05f7293df2e7b3df2d12a6a124c4338d0126b3e
yunkang <yunkang@yunkang>
2020-04-19 13:44:28 +0800

    modify in eclipse

--- src/com/jack/test/ken.java
+++ src/com/jack/test/ken.java
@@ -3,9 +3,7 @@ package com.jack.test;
 public class ken {
 
 	public static void main(String[] args) {
-//在云端修改创建冲突
-System.out.println("this is a conflict on web modify!");
-		System.out.println("test!");
+		//modify in eclipse1
 	}
 
 }
