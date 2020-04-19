ba908b9f49538f03d049b07d919a245abe9f241c
yunkang <yunkang@yunkang>
2020-04-19 14:00:30 +0800

    commit local file

--- src/com/jack/test/ken.java
+++ src/com/jack/test/ken.java
@@ -4,6 +4,8 @@ public class ken {
 
 	public static void main(String[] args) {
 		//modify in eclipse1
+		System.out.println("在本地修改和云端会出现冲突");
+		//modify in eclipse1
 		System.out.println("在云端修改和本地会出现冲突");
 	}
 
