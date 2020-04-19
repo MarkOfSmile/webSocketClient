d7f6fe4c40ac548114b56d6fb92070e20a33e435
MarkOfSmile <49064762+MarkOfSmile@users.noreply.github.com>
2020-04-19 13:20:06 +0800

    Update ken.java

--- src/com/jack/test/ken.java
+++ src/com/jack/test/ken.java
@@ -3,6 +3,8 @@ package com.jack.test;
 public class ken {
 
 	public static void main(String[] args) {
+//在云端修改创建冲突
+System.out.println("this is a conflict on web modify!");
 		System.out.println("test!");
 	}
 
